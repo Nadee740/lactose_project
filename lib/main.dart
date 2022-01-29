dba4876aeefd91099b1a1fce10f929bf2bee10fe
Nadeem740 <nadeemblayparambil@gmail.com>
2022-01-29 15:53:12 +0530

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:lactose_project/Screen/CurrAppointment.dart';
 import 'package:lactose_project/Screen/DoctorList.dart';
 import 'package:lactose_project/Screen/Home.dart';
@@ -9,8 +12,10 @@ import 'package:lactose_project/Screen/ShowDoctorDetails.dart';
 import 'package:lactose_project/Screen/ShowHospitals.dart';
 import 'package:lactose_project/Screen/ShowLabs.dart';
 import 'package:lactose_project/Screen/signup.dart';
+import 'package:http/http.dart' as http;
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
   runApp(const MyApp());
 }
 
@@ -21,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DoctorList(),
+      home: LoginPage(),
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           color: const Color(0xff17edf1),
@@ -29,4 +34,19 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+Future<bool> checkLogin()async{
+  final storage = new FlutterSecureStorage();
+
+  var url = "http://10.0.2.2:8000/users/me";
+  var token = await storage.read(key: "jwtToken");
+
+  var res = await http.get(Uri.parse(url), headers: <String, String>{
+    'Content-Type': 'application/json; charset=UTF-8',
+    'Authorization':'Bearer ${token}'
+  },);
+  // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWY0ZmY0NmI2ZGZmZDZiMmMzNmFiNGUiLCJpYXQiOjE2NDM0NDYwODZ9.ETka6u8ShfXmpMNW7dTX_dHsCzeRYhJ8d2yeYXey1u0
+  var responsebody=json.decode(res.body);
+  print(responsebody);
+  return responsebody['error']?false:true;
 }
